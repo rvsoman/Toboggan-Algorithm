2da9e67f03d10d09636c532f5141c2986e93f3e1
rvsoman <43816623+rvsoman@users.noreply.github.com>
2020-06-10 20:16:46 +0530

    Update toboggan_try.m

--- toboggan_try.m
+++ toboggan_try.m
@@ -1,79 +1,32 @@
-clc;clear;close all;
+clc; clear all; close all;
+%grad=input('enter gradient matrix');
 grad=[[12 14 23 13];[16 11 21 18];[21 24 23 12];[12 21 20 10]];
-img=[[4 4 3 2];[3 7 6 1];[2 7 6 2];[0 1 0 2]];
-grad1=padarray(grad,[1,1],999);
-[row,col]=size(img);
-final=zeros(row,col);
-c=zeros(9,0);
-x=999;
-r=1;
-cl=1;
-for ii=2:row+1
-    for jj=2:col+1
-       c(1)=grad1(ii-1,jj-1);
-       if x> c(1)
-           x=c(1);
-           r=ii-1;
-           cl=jj-1;
-       end
-           
-       c(2)=grad1(ii-1,jj);
-       if x> c(2)
-           x=c(2);
-           r=ii-1;
-           cl=jj;
-       end
-       c(3)=grad1(ii-1,jj+1);
-       if x> c(3)
-           x=c(3);
-           r=ii-1;
-           cl=jj+1;
-       end
-       c(4)=grad1(ii,jj-1);
-       if x> c(4)
-           x=c(4);
-           r=ii;
-           cl=jj-1;
-       end
-       c(5)=grad1(ii,jj);
-       if x> c(5)
-           x=c(5);
-           r=ii;
-           cl=jj;
-       end
-       c(6)=grad1(ii,jj+1);
-       if x> c(6)
-           x=c(6);
-           r=ii;
-           cl=jj+1;
-       end
-       c(7)=grad1(ii+1,jj-1);
-       if x> c(7)
-           x=c(7);
-           r=ii+1;
-           cl=jj-1;
-       end
-       c(8)=grad1(ii+1,jj);
-       if x> c(8)
-           x=c(8);
-           r=ii+1;
-           cl=jj;
-       end
-       c(9)=grad1(ii+1,jj+1);
-       if x> c(9)
-           x=c(9);
-           r=ii+1;
-           cl=jj+1;
-       end
-        
-        final(ii-1,jj-1)=img(r-1,cl-1);
-        
+%I=input('enter iamge matrix');
+I=[[4 4 3 2];[3 7 6 1];[2 7 6 2];[0 1 0 2]];
+%pad the gradient matrix with a large number
+new_grad = padarray(grad,[1 1],999);  
+[rows_new_grad,cols_new_grad]=size(new_grad);
+
+b=zeros(1,9);   
+arr=[];
+for j=2:cols_new_grad-1
+    for i=2:rows_new_grad-1
+        %get the neighbouring pixels for each pixel
+        b(5)=new_grad(i-1,j-1); b(6)=new_grad(i-1,j); 
+        b(7)=new_grad(i-1,j+1); b(4)=new_grad(i,j-1);
+        b(8)=new_grad(i,j+1); b(3)=new_grad(i+1,j-1); 
+        b(2)=new_grad(i+1,j); b(1)=new_grad(i+1,j+1);
+        b(9)=new_grad(i,j);
+        min_pixel=min(b);
+        loc=find(b==min_pixel); %location of minimum pixel
+        %get the locations of neighbouring pixels
+        row_arr=[i+1 i+1 i+1 i i-1 i-1 i-1 i i];
+        col_arr=[j+1 j j-1 j-1 j-1 j j+1 j+1 j];
+        R=row_arr(loc);
+        C=col_arr(loc);
+        t=new_grad(row_arr(loc),col_arr(loc));
+        im_t=I(row_arr(loc)-1,col_arr(loc)-1);
+        arr=[arr im_t];
     end
 end
-
-
-
-
-
-
-
+final_mat=reshape(arr,[4,4]);  %final matrix
